342bf13a3c80f99ef29b854296c03b520635c54c
Sung Paik <sungypaik@gmail.com>
2021-08-02 17:08:20 -0400

    created the product, order, category, and user models. also included associations

--- server/db/index.js
+++ server/db/index.js
@@ -5,7 +5,17 @@ const db = require('./db');
 const User = require('./models/User');
 const Product = require('./models/Product');
 const Order = require('./models/Order');
+const Category = require('./models/Category');
 //associations could go here!
+
+User.hasMany(Order);
+Order.belongsTo(User);
+
+Order.hasMany(Product, { through: 'product_orders' });
+Product.hasMany(Order, { through: 'product_orders' });
+
+Product.hasOne(Category);
+Category.belongsToMany(Product);
 
 module.exports = {
   db,
@@ -13,5 +23,6 @@ module.exports = {
     User,
     Product,
     Order,
+    Category,
   },
 };
