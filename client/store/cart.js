f3f78bd49105cee7385819c34ce19ef43361f79f
chrisbolosan <cbolosan@gmail.com>
2021-08-04 15:37:51 -0500

    cart store up, backend route for user cart id up, cart button and clickable link to cart established, front end user interface established, dummy data for users carts implented engineer MVP

---
+++ client/store/cart.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+//action type
+const SET_CART = 'SET_CART';
+
+//creator
+export const set_cart = (cart) => ({
+    type: SET_CART,
+    cart,
+})
+
+//thunker
+export const fetchCart = (id) => async(dispatch) => {
+    try {
+        const {data} = axios.get(`${id}/cart`)
+        dispatch(set_cart(data))
+    } catch (error) {
+        console.error(error);
+    }
+}
+//store
+export default function cartReducer(state = [], payload) {
+    switch (payload.type) {
+      case SET_CART:
+        return payload.cart;
+      case ADD_CART:
+        return [...state, payload.cart];
+      case DELETE_CART:
+        return state.filter((cart) => cart.id !== payload.cart.id);
+      case UPDATE_CART:
+      return state.map((cart) =>
+      (cart.id === payload.cart.id ? payload.cart : cart));
+      default:
+        return state;
+    }
