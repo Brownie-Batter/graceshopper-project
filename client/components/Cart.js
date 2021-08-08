5b85b5df9f2249f2c02f69394bcec1185ff5b3ce
malessi5 <mikealessi5@gmail.com>
2021-08-08 12:53:33 -0400

    delete cart button working

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchCart } from '../store/cart';
 import CartItem from './CartItem';
+import { deleteProductFromCart } from '../store/cart';
 
 function Cart(props) {
   const {
@@ -9,17 +10,26 @@ function Cart(props) {
       params: { id },
     },
     cart,
+    userId,
+    deleteProduct,
   } = props;
 
   useEffect(() => {
     props.fetchCart(id);
   }, []);
-   return (
-   
+  return (
     <div className="food-container">
       {cart.length ? (
         cart.map(({ name, id, orderDetails: { quantity, price } }) => (
-          <CartItem key={id} name={name} quantity={quantity} price={price} />
+          <CartItem
+            key={id}
+            productId={id}
+            name={name}
+            quantity={quantity}
+            price={price}
+            userId={userId}
+            deleteProduct={deleteProduct}
+          />
         ))
       ) : (
         <div>
@@ -32,9 +42,12 @@ function Cart(props) {
 
 const mapState = (state) => ({
   cart: state.cart,
+  userId: state.auth.id,
 });
 const mapDispatch = (dispatch) => ({
   fetchCart: (id) => dispatch(fetchCart(id)),
+  deleteProduct: (userId, productId) =>
+    dispatch(deleteProductFromCart(userId, productId)),
 });
 
 export default connect(mapState, mapDispatch)(Cart);
